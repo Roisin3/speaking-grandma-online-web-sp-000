2917b30b8394e9ac5528c2f7ab2fdd4c990625d8
Michael Lynch <michael@michaelrlynch.com>
2019-08-09 00:29:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
     puts "HUH?! SPEAK UP, SONNY!"
 #  elsif phrase == "I LOVE YOU GRANDMA!"
 #    puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938"
+#  elsif phrase == phrase.upcase
+#    puts "NO, NOT SINCE 1938"
   end
 end
